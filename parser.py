a3442b5bee3a24b650fcd680f0637f17104040c8
dlewis777 <lefranchboi@gmail.com>
2016-06-13 12:03:02 -0400

    added turkish parser
    parser running and working

--- parser.py
+++ parser.py
@@ -1,16 +1,18 @@
 from parser.parse_ja import generate_translation_tuples as ja_parser
 from parser.parse_vi import generate_translation_tuples as vi_parser
 from parser.helper import get_edition_from_url, get_html_tree
-
+from parser.parse_tr import generate_translation_tuples as tr_parser
 
 tested_url = [
     "https://vi.wiktionary.org/wiki/kh%C3%B4ng#Ti.E1.BA.BFng_Vi.E1.BB.87t",
     "https://vi.wiktionary.org/wiki/c%C3%A1m_%C6%A1n#Ti.E1.BA.BFng_Vi.E1.BB.87t",
     "https://ja.wiktionary.org/wiki/%E3%81%AA%E3%81%84",
     "https://ja.wiktionary.org/wiki/%E9%81%BA%E4%BC%9D%E5%AD%90",
+    "https://tr.wiktionary.org/wiki/ev",
+    "https://tr.wiktionary.org/wiki/abartmak",
 ]
 
-parsers = {'ja': ja_parser, 'vi': vi_parser}
+parsers = {'ja': ja_parser, 'vi': vi_parser, 'tr': tr_parser}
 
 
 def main():
