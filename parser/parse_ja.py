f7705555a9f9e7fcce826330502dd5d192daa1ad
Qian Xiong <qx2dk@virginia.edu>
2016-06-22 12:27:51 -0400

    generate pronunciation as well; use page word as the headword

--- parser/parse_ja.py
+++ parser/parse_ja.py
@@ -6,13 +6,13 @@ from parser.helper import get_html_tree_from_url
 class JaParser(GeneralParser):
     tested_url = [
         "https://ja.wiktionary.org/wiki/%E3%81%AA%E3%81%84",
-        "https://ja.wiktionary.org/wiki/%E9%81%BA%E4%BC%9D%E5%AD%90"
+        "https://ja.wiktionary.org/wiki/%E9%81%BA%E4%BC%9D%E5%AD%90",
+        "https://ja.wiktionary.org/wiki/speak",
     ]
 
     def __init__(self):
         super(JaParser, self).__init__()
         self.edition = 'ja'
-
 
     # override the parent class method
     def generate_translation_tuples(self, soup):
@@ -22,45 +22,62 @@ class JaParser(GeneralParser):
         :return: tuple of the form (edition, headword, head_lang, translation, trans_lang, trans_lang_code, part_of_speech)
         """
 
-        # START non-edition-specific
-        # this is the table of content which is present in each edition
-        toc = soup.find('div', id='mw-content-text')
-        # print(toc.get_text())
-        page_state = {'headword': None,
-                      'headword_lang': None,
-                      'part_of_speech': None,
-                      'translation_region': None}
-        for element in toc.children:
-            if isinstance(element, Tag):  # it could be a Tag or a NavigableString
+        # format the data to lists and yield
+        for word_data in self.parse_page(soup):
+            for translation_tup in word_data['translations']:
+                yield (
+                    self.edition, word_data['headword'], word_data['headword_lang'], translation_tup[0],
+                    translation_tup[1], translation_tup[2], word_data['part_of_speech'], word_data['pronunciation'])
+            else:  # when the translation list is empty
+                yield (self.edition, word_data['headword'], word_data['headword_lang'], '',
+                       '', '', word_data['part_of_speech'], word_data['pronunciation'])
+
+    def parse_page(self, soup):
+        """ Yield for each language section
+        """
+        # try:
+        #     page_heading = soup.find('div', class_='mw-body-content').previous_sibling.text
+        # except AttributeError as e:
+        #     print(soup)
+        page_content = soup.find('div', id='mw-content-text')
+        page_heading = None
+        element = soup.find('div', class_='mw-body-content') or page_content
+        if element is None:
+            return
+        while not page_heading:
+            element = element.previous_sibling
+            if isinstance(element, Tag):
+                page_heading = element.text
+
+        page_state = {'headword': page_heading,
+                      'headword_lang': "",
+                      'part_of_speech': "",
+                      'pronunciation': "",
+                      'translations': []}
+        for element in page_content.children:
+            if isinstance(element, Tag):
                 level = self.get_heading_level(element.name)
-                # END non-edition-specific
-                if level == 2:  # it is a header tag
+
+                if level == 2:
+                    if page_state['headword']:
+                        yield page_state
                     page_state['headword_lang'] = self.get_heading_text(element)
-                    page_state['translation_region'] = False
+                    page_state['part_of_speech'] = ""
+                    # page_state['headword'] = page_heading  # default value
+                    page_state['pronunciation'] = ""
+                    page_state['translations'] = []
                 elif level == 3:
                     page_state['part_of_speech'] = self.get_heading_text(element)
-                    page_state['translation_region'] = False
-                elif element.name == "p":  # is a paragraph tag
-                    bold_word = element.b
-                    if bold_word:
-                        page_state['headword'] = bold_word.get_text()
-                        # print("headword: ", bold_word.get_text().strip())
-                        page_state['translation_region'] = True
-                    else:
-                        page_state['translation_region'] = False
-
-                elif element.name == "table":
-                    # TBD
-                    # if "translations" in element['class']:
-                    if True:
-                        for translation, lang, lang_code in self.parse_translation_table(element):
-                            yield (
-                                self.edition, page_state['headword'] or "NO_HEADWORD", page_state['headword_lang'],
-                                translation,
-                                lang, lang_code,
-                                page_state['part_of_speech'])
-                else:
-                    page_state['translation_region'] = False
+                # elif element.name == "p":
+                #     bold_word = element.b
+                #     if bold_word:
+                #         page_state['headword'] = bold_word.get_text()
+                elif element.name == 'table' and 'class' in element.attrs and 'translations' in element['class']:
+                    page_state['translations'] += list(self.parse_translation_table(element))
+                pronunciation = element.find(class_="IPA")
+                if pronunciation:
+                    page_state['pronunciation'] = pronunciation.text
+        yield page_state
 
 
 def main():
@@ -69,6 +86,7 @@ def main():
         soup = get_html_tree_from_url(url)
         for tup in parser.generate_translation_tuples(soup):
             print(','.join(tup))
+            # print(tup)
 
 
 if __name__ == '__main__':
