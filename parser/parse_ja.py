08573aa86fd5575905f89b3c73b36f720cbebd27
Qian Xiong <qx2dk@virginia.edu>
2016-06-22 15:08:06 -0400

    get the correct pos for translations by using dict with pos as key and a list of translation as value

--- parser/parse_ja.py
+++ parser/parse_ja.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from bs4 import Tag
 from parser.general import GeneralParser
 from parser.helper import get_html_tree_from_url
@@ -26,13 +28,18 @@ class JaParser(GeneralParser):
         for word_data in self.parse_page(soup):
             if word_data is None:
                 continue
-            for translation_tup in word_data['translations']:
-                yield (
-                    self.edition, word_data['headword'], word_data['headword_lang'], translation_tup[0],
-                    translation_tup[1], translation_tup[2], word_data['part_of_speech'], word_data['pronunciation'])
-            else:  # when the translation list is empty
-                yield (self.edition, word_data['headword'], word_data['headword_lang'], '',
-                       '', '', word_data['part_of_speech'], word_data['pronunciation'])
+
+            yield (self.edition, word_data['headword'], word_data['headword_lang'], '',
+                   '', '', ';'.join(word_data['part_of_speech'][1:]), word_data['pronunciation'])
+            for pos, translation_tup_list in word_data['translations'].items():
+                for translation_tup in translation_tup_list:
+                    yield (
+                        self.edition, word_data['headword'], word_data['headword_lang'], translation_tup[0],
+                        translation_tup[1], translation_tup[2], pos, word_data['pronunciation'])
+                if not translation_tup_list:  # a part of speech doesn't have any translations
+                    if pos != '':
+                        yield (self.edition, word_data['headword'], word_data['headword_lang'], '',
+                               '', pos, word_data['pronunciation'])
 
     def parse_page(self, soup):
         """ Yield for each language section
@@ -52,32 +59,37 @@ class JaParser(GeneralParser):
                 page_heading = element.text
 
         page_state = {'headword': None,
-                      'headword_lang': "",
-                      'part_of_speech': "",
-                      'pronunciation': "",
-                      'translations': []}
+                      'headword_lang': '',
+                      'part_of_speech': [''],
+                      'pronunciation': '',
+                      'translations': defaultdict(lambda: defaultdict(list))}
         for element in page_content.children:
             if isinstance(element, Tag):
+                pronunciation = element.find(class_="IPA")
+                if pronunciation:
+                    page_state['pronunciation'] = pronunciation.text
+
                 level = self.get_heading_level(element.name)
                 if level == 2:
                     if page_state['headword']:
                         yield page_state
                     page_state['headword_lang'] = self.get_heading_text(element)
-                    page_state['part_of_speech'] = ""
+                    page_state['part_of_speech'] = ['']
                     page_state['headword'] = page_heading  # default value
-                    page_state['pronunciation'] = ""
-                    page_state['translations'] = []
+                    page_state['pronunciation'] = ''
+                    page_state['translations'] = defaultdict(list)
                 elif level == 3:
-                    page_state['part_of_speech'] = self.get_heading_text(element)
+                    page_state['part_of_speech'].append(self.get_heading_text(element))
                 # elif element.name == "p":
                 #     bold_word = element.b
                 #     if bold_word:
                 #         page_state['headword'] = bold_word.get_text()
                 elif element.name == 'table' and 'class' in element.attrs and 'translations' in element['class']:
-                    page_state['translations'] += list(self.parse_translation_table(element))
-                pronunciation = element.find(class_="IPA")
-                if pronunciation:
-                    page_state['pronunciation'] = pronunciation.text
+                    translation_tup_list = list(self.parse_translation_table(element))
+                    if not translation_tup_list:
+                        continue
+                    pos = page_state['part_of_speech'][-1]
+                    page_state['translations'][pos] += translation_tup_list
 
         if page_state['headword']:
             yield page_state
